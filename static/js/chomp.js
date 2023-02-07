2f818f5a1307abf8cff6af8c8b2f26c4df194697
nikhilNathwani <nnathwani36@gmail.com>
2023-02-06 22:30:17 -0800

    Added capability to click on a chocolate and 'chomp' (aka hide) all the chocolates to the right of it and beneath it! A green dot is still visible for each chocolate, this is the remaining border of each 0px-sidelength rectangle, so I need the hide function to remove the border too. Can fix later

--- static/js/chomp.js
+++ static/js/chomp.js
@@ -1,6 +1,13 @@
+//Canvas vars
 let width= 1200;
 let height= 600;
+
+//Chocolate vars
 let chocLength= 80;
+
+//Board vars
+let numRows= 4;
+let numColumns= 5;
 let board= [5,5,5,5];
 let grid= [];
 
@@ -25,6 +32,24 @@ function draw() {
     for(let i=0; i<board.length; i++) {
         for(let j=0; j<board[i]; j++) {
             grid[i][j].show();
+        }
+    }
+}
+
+function mouseClicked() {
+   x= floor(mouseX/chocLength)
+   y= floor(mouseY/chocLength)
+   console.log("Hi",x,y);
+   chomp(x,y);
+}
+
+function chomp(x,y) {
+    for(let row=y; row<numRows; row++) {
+        if(grid[row] == 0) {
+            continue;
+        }
+        for(let column=x; column<numColumns; column++) {
+            grid[row][column].hide();
         }
     }
 }

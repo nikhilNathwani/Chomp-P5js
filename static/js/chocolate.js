2cdca7e430146bcca810c18cce979d65e417fd5d
nikhilNathwani <nnathwani36@gmail.com>
2023-02-06 22:07:32 -0800

    Drew 4x5 grid of 'chocolate' (it's just blue squares with green border at the moment

---
+++ static/js/chocolate.js
@@ -0,0 +1,11 @@
+
+function Chocolate(x, y, length) {
+    this.x = x;
+    this.y = y;
+    this.length= length; 
+
+    this.show= function() {
+        fill(0,0,255);
+        rect(this.x,this.y,this.length,this.length);
+    }
+}
--- static/js/chomp.js
+++ static/js/chomp.js
@@ -1,7 +1,30 @@
+let width= 1200;
+let height= 600;
+let chocLength= 80;
+let board= [5,5,5,5];
+let grid= [];
+
 function setup() {
-    createCanvas(400, 400);
-  }
+    createCanvas(width, height);
+    choc= new Chocolate(0,0,chocLength);
+
+    for(let i=0; i<board.length; i++) {
+        let row= [];
+        for(let j=0; j<board[i]; j++) {
+            let choc= new Chocolate(j*chocLength,i*chocLength,chocLength);
+            row.push(choc);
+        }
+        grid.push(row);
+    }
+}
   
-  function draw() {
-    background(0,0,255);
-  }
+function draw() {
+    background(0,0,0);
+    stroke(0,255,0);
+ 
+    for(let i=0; i<board.length; i++) {
+        for(let j=0; j<board[i]; j++) {
+            grid[i][j].show();
+        }
+    }
+}
